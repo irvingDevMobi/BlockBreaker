d4bdebe959810a02a899e5cc80dab8ca3ab4b5c4
Irving Lóp <irving.lopez@unam.mobi>
2015-07-07 17:51:29 -0500

    new scripts added

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+    private float mousePosition;
+    private Vector3 paddlePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Input.mousePosition.x / Screen.width * 16.0f;
+        paddlePosition = new Vector3(Mathf.Clamp(mousePosition, 0.5f, 15.5f), 
+                                     this.transform.position.y,
+                                     this.transform.position.z);
+        this.transform.position = paddlePosition;
+        //print(mousePosition);
+	}
+}
