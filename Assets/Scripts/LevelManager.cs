417f03244a3c70b42de4cf59d32b31b077c9902f
Irving Lóp <irving.lopez@unam.mobi>
2015-07-06 22:06:42 -0500

    init commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+        //Debug.Log ("Level load requested for: " + name);
+        Application.LoadLevel(name);
+	}
+
+    public void RequestQuit()
+    {
+        Debug.Log("I wan to exit!!!");
+        Application.Quit();
+    }
+}
