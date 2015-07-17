b48b3e796a587a7b8798b2d7cdfd6453baa805f3
Irving Lóp <irving.lopez@unam.mobi>
2015-07-17 08:28:34 -0500

    Changes lost added

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -13,11 +13,22 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+#if UNITY_ANDROID
+        mousePosition = Input.acceleration.x;
+        transform.Translate(new Vector3(mousePosition * 0.5f, 0));
+#else
+
+
         mousePosition = Input.mousePosition.x / Screen.width * 16.0f;
-        paddlePosition = new Vector3(Mathf.Clamp(mousePosition, 0.5f, 15.5f), 
+        paddlePosition = new Vector3(Mathf.Clamp(mousePosition, 0.5f, 15.5f),
                                      this.transform.position.y,
                                      this.transform.position.z);
         this.transform.position = paddlePosition;
+
+#endif
+        
         //print(mousePosition);
 	}
 }
